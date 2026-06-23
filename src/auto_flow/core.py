78555547ccc6918bb2347d005b33e910c6ed5a5b
MinhPrompt <minhlv@socymedia.com.vn>
2026-06-23 08:19:17 +0700

    update choose method setup button

--- src/auto_flow/core.py
+++ src/auto_flow/core.py
@@ -1,41 +1,50 @@
+import re
+
 from playwright.sync_api import sync_playwright, TimeoutError, Locator
 from playwright.sync_api._generated import Page
 from src.auto_flow.config import THIS_PROFILE, OUTPUT_DATA_DIR
 from src.auto_flow.loggers import main_logger
 from src.auto_flow.utils.prompts_reader import script_prompts
+from src.auto_flow.utils.helpers import debug_locator
 
 GOOGLE_FLOW_URL = 'https://labs.google/fx/vi/tools/flow'
 PROJECT_NAME = 'testproject'
-GEN_IMAGE_TIME_SLEEP = 15 * 1000 # ms
+GEN_IMAGE_TIME_SLEEP = 15 * 1000  # ms
 this_scene = script_prompts.scenes[0]
 
+
 def create_images(page: Page):
-    # Nếu chế độ tạo video đang bật thì chuyển thành chế độ tạo ảnh
-    setup_video_btn = page.get_by_role("button", name="Video · 8s crop_16_9 1x")
-    if setup_video_btn.is_visible():
-        setup_video_btn.click()
-        page.get_by_role("tab", name="image Hình ảnh").click()
-        page.get_by_role("tab", name="1x")
-        page.get_by_role("button", name="🍌 Nano Banana 2 arrow_drop_down").click()
+    # Chuyển thành chế độ tạo ảnh
+    setup_btn = page.locator("button").filter(
+        has_text=re.compile(r"(Nano Banana|Video)")
+    )
+    if setup_btn.is_visible():
+        setup_btn.click()
+        btn_image_tab = page.get_by_role("tab", name="image Hình ảnh")
+        btn_image_tab.wait_for()
+        btn_image_tab.click()
+        page.get_by_role("tab", name="1x").click()
+        model_drop_down_btn = page.get_by_role("button").filter(
+            has_text="arrow_drop_down"
+        )
+        model_drop_down_btn.click()
         page.get_by_role("button", name="🍌 Nano Banana 2")
         page.keyboard.press('Escape')
 
-    # 
-
-    page.pause()
-
-    # Nếu đang ở chế độ tạo ảnh sẵn rồi thì tiến hành tạo ảnh
-
+    # Tiến hành tạo ảnh
     for pair_prompt in this_scene.pair_prompts:
         page.get_by_role("paragraph").filter(has_text="Bạn muốn tạo gì?").fill(pair_prompt.image)
         page.get_by_role("button", name="arrow_forward Tạo").click()
         page.wait_for_timeout(GEN_IMAGE_TIME_SLEEP)
 
+
 def create_videos(page: Page):
-    # Nếu chế độ tạo ảnh đang bật thì chuyển sang chế độ tạo video
-    setup_image_btn = page.get_by_role("button", name="🍌 Nano Banana 2 crop_16_9 1x")
-    if setup_image_btn.is_visible():
-        setup_image_btn.click()
+    # Chuyển thành chế độ tạo video
+    setup_btn = page.locator("button").filter(
+        has_text=re.compile(r"(Nano Banana|Video)")
+    )
+    if setup_btn.is_visible():
+        setup_btn.click()
 
         create_video_mod_btn = page.get_by_role("tab", name="play_circle Video")
         create_video_mod_btn.wait_for()
@@ -50,10 +59,10 @@ def create_videos(page: Page):
         chon_so_luong = page.get_by_role("tab", name="1x")
         chon_so_luong.click()
 
-        page.pause()
-
-        danh_sach_mo_hinh_btn = page.locator('button[aria-haspopup="menu"]')
-        danh_sach_mo_hinh_btn.click()
+        model_drop_down_btn = page.get_by_role("button").filter(
+            has_text="arrow_drop_down"
+        )
+        model_drop_down_btn.click()
 
         chon_mo_hinh_btn = page.get_by_role("button", name="volume_up Veo 3.1 - Lite [")
         chon_mo_hinh_btn.wait_for()
@@ -67,23 +76,20 @@ def create_videos(page: Page):
 
     page.pause()
 
-
-
-
-
     pass
+
 
 def main_script():
     with (sync_playwright() as p):
-        context = p.chromium.launch_persistent_context( # Khởi tạo context
-            user_data_dir=THIS_PROFILE, # Sử dụng 1 profile
-            headless=False, # Không ẩn trình duyệt
-            channel='chrome', # Dùng kênh chrome
-            accept_downloads=True, # Cho phép downdload
+        context = p.chromium.launch_persistent_context(  # Khởi tạo context
+            user_data_dir=THIS_PROFILE,  # Sử dụng 1 profile
+            headless=False,  # Không ẩn trình duyệt
+            channel='chrome',  # Dùng kênh chrome
+            accept_downloads=True,  # Cho phép downdload
             # downloads_path=OUTPUT_DATA_DIR # Folder download mặc định
         )
-        page = context.new_page() # Tạo 1 page
-        page.goto(GOOGLE_FLOW_URL, wait_until='networkidle') # Di chuyển đến URL FLOW
+        page = context.new_page()  # Tạo 1 page
+        page.goto(GOOGLE_FLOW_URL, wait_until='networkidle')  # Di chuyển đến URL FLOW
 
         # Nếu có dự án với tên đó rồi thì vào, không thì tạo mới
         project = page.locator("div:has-text('testproject') > a")
@@ -93,21 +99,18 @@ def main_script():
         else:
             ## Tạo mới 1 dự án và đặt tên
             print('Tạo mới 1 dự án.')
-            page.locator('#__next > div.sc-c7ee1759-1.jhwuTJ > div > div > button').click() # Nút tạo mới project
+            page.locator('#__next > div.sc-c7ee1759-1.jhwuTJ > div > div > button').click()  # Nút tạo mới project
             print('Đặt tên dự án.')
             page.get_by_role("textbox", name="Văn bản có thể chỉnh sửa").fill(PROJECT_NAME)
             page.get_by_role("button", name="done Xong").click()
 
-
-
-        #Kiểm tra và tắt chế độ tác nhân nếu đang bật
+        # Kiểm tra và tắt chế độ tác nhân nếu đang bật
         tac_nhan_btn = page.get_by_role("button", name="Tác nhân", exact=True)
         pressed = tac_nhan_btn.get_attribute("aria-pressed")
-        page.pause()
         if pressed == 'true':
             tac_nhan_btn.click()
 
-        create_images(page)
+        # create_images(page)
         create_videos(page)
 
         print("Xóa dự án.")
--- src/auto_flow/utils/helpers.py
+++ src/auto_flow/utils/helpers.py
@@ -1,6 +1,38 @@
 import json
 from pathlib import Path
+from playwright.sync_api import Locator
 
 def read_json(file_path: Path) -> dict:
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
+
+
+def debug_locator(locator: Locator):
+    print(f"\n🔍 Total: {locator.count()} elements\n")
+
+    for i in range(locator.count()):
+        el = locator.nth(i)
+
+        print(f"\n===== [{i}] =====")
+
+        print("TEXT:")
+        print(el.text_content())
+
+        print("\nINNER TEXT:")
+        print(el.inner_text())
+
+        print("\nTAG:")
+        print(el.evaluate("e => e.tagName"))
+
+        print("\nATTRIBUTES:")
+        print("class:", el.get_attribute("class"))
+        print("id:", el.get_attribute("id"))
+        print("role:", el.get_attribute("role"))
+        print("aria-label:", el.get_attribute("aria-label"))
+        print("aria-haspopup:", el.get_attribute("aria-haspopup"))
+
+        print("\nOUTER HTML (preview):")
+        html = el.evaluate("e => e.outerHTML")
+        print(html[:300])  # cắt để khỏi spam console
+
+        print("\n--------------------")
